9ae7a227395cef2948740f7da9c0d5df7f0ed2a2
Bartosz Elert <elert.bartosz@gmail.com>
2025-03-04 23:21:02 +0100

    added readiness endpoint

--- main.go
+++ main.go
@@ -5,12 +5,22 @@ import (
 	"net/http"
 )
 
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write([]byte("OK")); err != nil {
+		log.Printf("Something went wrong while writing the response: %v", err)
+	}
+
+}
+
 func main() {
 	filepathRoot := "."
-	port := "8080"
+	port := ":8080"
 
 	serverMux := http.NewServeMux()
-	serverMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	serverMux.HandleFunc("/healthz", handlerHealthz)
 	server := http.Server{
 		Addr:    port,
 		Handler: serverMux,
